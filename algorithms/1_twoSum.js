61cc40a998fd8dcf41563aa31f17097da6467066
Neil Murphy <wnmurphy@gmail.com>
2017-04-03 17:41:01 -0700

    Add a O(n) solution to 1_twoSum using hash tables instead of arrays.

--- algorithms/1_twoSum.js
+++ algorithms/1_twoSum.js
@@ -12,7 +12,7 @@
 */
 
 // Brute Force - Nested Loops
-var twoSum = function(nums, target) {
+var twoSumBruteForce = function(nums, target) {
    var result;
    nums.forEach(function(num1, idx1){
        nums.forEach(function(num2, idx2){
@@ -23,4 +23,36 @@ var twoSum = function(nums, target) {
        });
    });
    return result;
+};
+
+// Two-Pass Hash Table
+// We can trade space for time performance and use the constant time access of hash tables to improve the algorithm.
+// Instead of nesting loops, we can have two separate passes of a storage hash table.
+// This solution only works if there are no repeating values in the array; to get a constant time lookup the numbers can only be stored as keys, and keys must be unique.
+// Another limitation of this solution is that, since there is no way to enforce iteration order over an object's keys, there is no way to enforce the order of array indices returned in the result tuple.
+var twoSum = function (nums, target) {
+  var numbers = {}, complement, result;
+  // First we map the numbers/indices in the array to an object as key-value pairs.
+  // [2, 7, 11, 15]
+  nums.map(function(num, idx){
+    numbers[num] = idx; 
+    // { 
+    //  "2": 0,
+    //  "7": 1, 
+    //  "11": 2, 
+    //  "15": 3 
+    // }
+  });
+  // Then we can loop over the storage hash...
+  for (var num in numbers) {
+    // ...and dynamically define the current complement we're searching for.
+    complement = target - num;
+    // If the complement exists as a key in the numbers object,
+    if (complement in numbers) {
+      // We can just return an array that contains the two indices we stored as values in the numbers object.
+      result = [numbers[complement], numbers[num]];
+      break;
+    }
+  }
+  return result;
 };
